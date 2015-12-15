b0e09bd1fb8ef099fee1017079a8a8c01f7ae026
Jose <jose@alsur.es>
2015-12-15 17:02:33 +0100

    modifico metodo blacklistedEmailInfo para agregar emails que estan en la lista negra por el usuario

--- plugins/restapi/includes/subscribers.php
+++ plugins/restapi/includes/subscribers.php
@@ -8,21 +8,21 @@ class Subscribers {
 
     /**
      * <p>Get all the Subscribers in the system.</p>
-		 * <p><strong>Parameters:</strong><br/>
-		 * [order_by] {string} name of column to sort, default "id".<br/>
-		 * [order] {string} sort order asc or desc, default: asc.<br/>
-		 * [limit] {integer} limit the result, default 100.<br/>
-		 * </p>
+     * <p><strong>Parameters:</strong><br/>
+     * [order_by] {string} name of column to sort, default "id".<br/>
+     * [order] {string} sort order asc or desc, default: asc.<br/>
+     * [limit] {integer} limit the result, default 100.<br/>
+     * </p>
      * <p><strong>Returns:</strong><br/>
      * List of Subscribers.
      * </p>
      */
     static function subscribersGet( $order_by='id', $order='asc', $limit=100 ) {
 
-				//getting optional values
-				if ( isset( $_REQUEST['order_by'] ) && !empty( $_REQUEST['order_by'] ) ) $order_by = $_REQUEST['order_by'];
-				if ( isset( $_REQUEST['order'] ) && !empty( $_REQUEST['order'] ) ) $order = $_REQUEST['order'];
-				if ( isset( $_REQUEST['limit'] ) && !empty( $_REQUEST['limit'] ) ) $limit = $_REQUEST['limit'];
+        //getting optional values
+        if ( isset( $_REQUEST['order_by'] ) && !empty( $_REQUEST['order_by'] ) ) $order_by = $_REQUEST['order_by'];
+        if ( isset( $_REQUEST['order'] ) && !empty( $_REQUEST['order'] ) ) $order = $_REQUEST['order'];
+        if ( isset( $_REQUEST['limit'] ) && !empty( $_REQUEST['limit'] ) ) $limit = $_REQUEST['limit'];
 
         Common::select( 'Users', "SELECT * FROM " . $GLOBALS['usertable_prefix'] . "user ORDER BY $order_by $order LIMIT $limit;" );
     }
@@ -81,10 +81,10 @@ class Subscribers {
             $stmt->bindParam("rssfrequency", $_REQUEST['rssfrequency']);
             $stmt->bindParam("password", $_REQUEST['password']);
             $stmt->bindParam("disabled", $_REQUEST['disabled']);
-            
+
             // fails on strict
 #            $stmt->bindParam("uniqid", md5(uniqid(mt_rand())));
-            
+
             $uniq = md5(uniqid(mt_rand()));
             $stmt->bindParam("uniqid", $uniq);
             $stmt->execute();
@@ -97,21 +97,21 @@ class Subscribers {
 
     }
 
-		/**
-		 * <p>Updates one Subscriber to the system.</p>
-		 * <p><strong>Parameters:</strong><br/>
-		 * [*id] {integer} the ID of the Subscriber.<br/>
-		 * [*email] {string} the email address of the Subscriber.<br/>
-		 * [*confirmed] {integer} 1=confirmed, 0=unconfirmed.<br/>
-		 * [*htmlemail] {integer} 1=html emails, 0=no html emails.<br/>
-		 * [*rssfrequency] {integer}<br/>
-		 * [*password] {string} The password to this Subscriber.<br/>
-		 * [*disabled] {integer} 1=disabled, 0=enabled<br/>
-		 * </p>
-		 * <p><strong>Returns:</strong><br/>
-		 * The updated Subscriber.
-		 * </p>
-		 */
+    /**
+     * <p>Updates one Subscriber to the system.</p>
+     * <p><strong>Parameters:</strong><br/>
+     * [*id] {integer} the ID of the Subscriber.<br/>
+     * [*email] {string} the email address of the Subscriber.<br/>
+     * [*confirmed] {integer} 1=confirmed, 0=unconfirmed.<br/>
+     * [*htmlemail] {integer} 1=html emails, 0=no html emails.<br/>
+     * [*rssfrequency] {integer}<br/>
+     * [*password] {string} The password to this Subscriber.<br/>
+     * [*disabled] {integer} 1=disabled, 0=enabled<br/>
+     * </p>
+     * <p><strong>Returns:</strong><br/>
+     * The updated Subscriber.
+     * </p>
+     */
     static function subscriberUpdate(){
 
         $sql = "UPDATE " . $GLOBALS['usertable_prefix'] . "user SET email=:email, confirmed=:confirmed, htmlemail=:htmlemail, rssfrequency=:rssfrequency, password=:password, passwordchanged=now(), disabled=:disabled WHERE id=:id;";
@@ -135,15 +135,15 @@ class Subscribers {
 
     }
 
-		/**
-		 * <p>Deletes a Subscriber.</p>
-		 * <p><strong>Parameters:</strong><br/>
-		 * [*id] {integer} the ID of the Subscriber.<br/>
-		 * </p>
-		 * <p><strong>Returns:</strong><br/>
-		 * The deleted Subscriber ID.
-		 * </p>
-		 */
+    /**
+     * <p>Deletes a Subscriber.</p>
+     * <p><strong>Parameters:</strong><br/>
+     * [*id] {integer} the ID of the Subscriber.<br/>
+     * </p>
+     * <p><strong>Returns:</strong><br/>
+     * The deleted Subscriber ID.
+     * </p>
+     */
     static function subscriberDelete(){
 
         $sql = "DELETE FROM " . $GLOBALS['usertable_prefix'] . "user WHERE id=:id;";
@@ -159,22 +159,22 @@ class Subscribers {
         }
 
     }
-	
-	/**
+
+    /**
      * <p>Subscribers assigned to a list.</p>
      * <p><strong>Parameters:</strong><br/>
      * [*user_id] {integer} the list-ID.
      * <p><strong>Returns:</strong><br/>
      * Array of subscribers assigned to a list.
      * </p>
-	 * @author: Jose <jose@alsur.es>
+     * @author: Jose <jose@alsur.es>
      */
     static function subscribersList ( $list_id=0 ) {
         $response = new Response();
         if ( $list_id==0 ) {
-			if (isset($_REQUEST['list_id'])) $list_id = $_REQUEST['list_id'];
-			else die('Sin lista no hay subscriptores');
-		}
+            if (isset($_REQUEST['list_id'])) $list_id = $_REQUEST['list_id'];
+            else die('Sin lista no hay subscriptores');
+        }
         $sql = "SELECT * FROM " . $GLOBALS['usertable_prefix'] . "user WHERE id IN (SELECT userid FROM " . $GLOBALS['table_prefix'] . "listuser WHERE listid=" . $list_id . ") ORDER BY id;";
         try {
             $db = PDO::getConnection();
@@ -188,19 +188,19 @@ class Subscribers {
         }
         die(0);
     }
-	
-	/**
+
+    /**
      * <p>modifico atributo a traves de la API.
-	 * {"9": "Pepito", "11": "Perez"}
-	 * No implementado en un primer momento.
+     * {"9": "Pepito", "11": "Perez"}
+     * No implementado en un primer momento.
      * </p>
-	 * @author: Jose <jose@alsur.es>
+     * @author: Jose <jose@alsur.es>
      */
     static function subscriberUpdateAttribute ($_data = array()) {
-		
-		# no esta depurada llamando a esta funcion directamente por $_REQUEST.
-		# if(!isset($_data)) $_data = $_REQUEST; 
-		
+
+        # no esta depurada llamando a esta funcion directamente por $_REQUEST.
+        # if(!isset($_data)) $_data = $_REQUEST;
+
         $sql = "REPLACE INTO " . $GLOBALS['usertable_prefix'] . "user_attribute SET attributeid=:attributeid, userid=:userid, value=:value";
         try {
             $db = PDO::getConnection();
@@ -214,22 +214,22 @@ class Subscribers {
             Response::outputError($e);
         }
     }
-	
-	/**
+
+    /**
      * <p>modifico json de atributos a traves de la API.
-	 * {"9": "Pepito", "11": "Perez"}
-	 * No implementado en un primer momento.
+     * {"9": "Pepito", "11": "Perez"}
+     * No implementado en un primer momento.
      * </p>
-	 * @author: Jose <jose@alsur.es>
+     * @author: Jose <jose@alsur.es>
      */
     static function subscriberUpdateAttributes () {
-		$userid = $_REQUEST['userid'];
-		
-		$metadata = json_decode(stripslashes($_REQUEST['metadata']), true);
-		foreach($metadata as $kd=>$vd){
-			
-			self::subscriberUpdateAttribute(array('userid' => $userid, 'attributeid' => $kd, 'value' => utf8_decode($vd)));
-		}
+        $userid = $_REQUEST['userid'];
+
+        $metadata = json_decode(stripslashes($_REQUEST['metadata']), true);
+        foreach($metadata as $kd=>$vd){
+
+            self::subscriberUpdateAttribute(array('userid' => $userid, 'attributeid' => $kd, 'value' => utf8_decode($vd)));
+        }
         Subscribers::SubscriberGet( $userid);
         die(0);
     }
@@ -244,7 +244,18 @@ class Subscribers {
         $sql = "SELECT ". $GLOBALS['usertable_prefix'] . "blacklist.email, added, `data` as reason FROM "
             . $GLOBALS['usertable_prefix'] . "blacklist INNER JOIN ".$GLOBALS['usertable_prefix'] ."blacklist_data"
             . " ON ".$GLOBALS['usertable_prefix'] . "blacklist.email=".$GLOBALS['usertable_prefix'] ."blacklist_data.email"
-            ." WHERE ".$GLOBALS['usertable_prefix']."blacklist.email = '".$email."'";
+            ." WHERE ".$GLOBALS['usertable_prefix']."blacklist.email = '".$email."'"
+            . "
+			UNION
+			(
+				SELECT email, null, 'Blacklist by profile user'
+				FROM " . $GLOBALS['usertable_prefix'] . "user WHERE blacklisted=1 AND email = '".$email."'
+			)
+			LIMIT 1
+			"
+
+        ;
+
 
         try {
             $db = PDO::getConnection();
@@ -267,7 +278,7 @@ class Subscribers {
 
 
     }
-	
-	
+
+
 
 }
